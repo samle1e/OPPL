34cd3ebdf88dbebf8e86ed806410e888d56a3d69
samle1e <sam.le@sba.gov>
2023-04-17 17:00:40 -0400

    Update Vendor_Lookup.py
    enter a UEI of a firm that never got contracts

--- Vendor_Lookup.py
+++ Vendor_Lookup.py
@@ -100,11 +100,15 @@ def vendor_id (data1, data2):
         df_for_in = session.create_dataframe(UEI_list + DUNS_list, schema=["col1"])
         data1=data1.filter((data1["VENDOR_UEI"].isin(df_for_in)) | 
                      (data1["VENDOR_DUNS_NUMBER"].isin(df_for_in)))
-        last_fy = data1.select(max(col("FISCAL_YEAR"))).to_pandas().iloc[0,0]
-        last_fy = int(last_fy)
-
-        data2=data2.filter((col("DATE_SIGNED") > datetime (last_fy, 9, 30, 0, 0)) &
-                            (data2["VENDOR_UEI_NUMBER"].isin(df_for_in)))
+        
+        try:
+            last_fy = data1.select(max(col("FISCAL_YEAR"))).to_pandas().iloc[0,0]
+            last_fy = int(last_fy)
+            data2=data2.filter((col("DATE_SIGNED") > datetime (last_fy, 9, 30, 0, 0)) &
+                                (data2["VENDOR_UEI_NUMBER"].isin(df_for_in)))
+        except: 
+            data1 = None
+            data2 = None
     else:
         data1 = None
         data2 = None
